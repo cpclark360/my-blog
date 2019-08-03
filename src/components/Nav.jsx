08ba2ce112f82e6c8e6850d1fd55813aa68240ab
cpclark360 <40728556+cpclark360@users.noreply.github.com>
2019-08-02 20:38:02 -0500

    k

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,6 +7,7 @@ class Nav extends Component {
     return (
       <nav>
         <div className="wrapper">
+<<<<<<< HEAD
           <div className="box menu" />
           <div className="box">
             <div className="icons">icons</div>
@@ -14,6 +15,11 @@ class Nav extends Component {
           <div className="box log">
             <input id="nav-search" type="search" placeholder="Search" />
           </div>
+=======
+          <div className="box menu">Menu</div>
+          <div className="box search">Search</div>
+          <div className="box log">Log</div>
+>>>>>>> parent of b4d0d96... updated
         </div>
       </nav>
     );

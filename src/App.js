eaa073e48c7066477857f8f2a8bb3cfaf5bbdf89
cpclark360 <40728556+cpclark360@users.noreply.github.com>
2019-08-15 20:41:57 -0500

    added dependecies
    underscore & mongoose

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from "./components/Nav";
 import Main from "./components/Main";
 import Aside from "./components/Aside";
 import Section from "./components/Section";
+const _ = require("underscore");
 
 class App extends Component {
   render() {
